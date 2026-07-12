8e32f2daaebfe0921d876e772706e9f17eb6bb88
Muskan Tariq <muskantariq2003@gmail.com>
2026-07-12 18:19:18 +0500

    added 2 languages

--- model.py
+++ model.py
@@ -4,16 +4,35 @@ import os
 from youtube_transcript_api import YouTubeTranscriptApi
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from urllib.parse import urlparse, parse_qs
+from youtube_transcript_api._errors import NoTranscriptFound
 
 load_dotenv()
 
 yt_api = YouTubeTranscriptApi()
 
-transcript = yt_api.fetch(input('Enter Video ID: '))
+def extract_video_id(url):
+    parsed_url = urlparse(url)
+
+    if parsed_url.hostname in ["www.youtube.com", "youtube.com"]:
+        return parse_qs(parsed_url.query).get("v", [None])[0]
+
+    elif parsed_url.hostname == "youtu.be":
+        return parsed_url.path.lstrip("/")
+
+    return None
+
+url = input("Paste URL: ")
+
+try:
+    transcript = yt_api.fetch(extract_video_id(url), languages=["en"])
+    print("Using English transcript")
+
+except NoTranscriptFound:
+    transcript = yt_api.fetch(extract_video_id(url), languages=["hi"])
+    print("Using Hindi transcript")
 
 full_text = "".join(snippet.text for snippet in transcript)
-# print(full_text)
-
 
 parser = StrOutputParser()
 
@@ -40,7 +59,3 @@ while True:
     })
 
     print('AI: ', result)
-
-# print(transcript)
-
-# llm.invoke('What is your name?')
